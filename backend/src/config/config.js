80c599a17cbebdb8803fa412bb2fd85d5e3ee4aa
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-05-24 03:04:32 -0300

    atualização config e jwt

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,10 +1,14 @@
 // src/config/config.js
+
 const config = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  host: process.env.MYSQL_HOST, dialect: 'mysql',
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT || 3306,
+  dialect: 'mysql',
 };
+
 module.exports = {
   development: config,
   test: config,
